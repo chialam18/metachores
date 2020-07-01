3dbf2b56f42fd328a458bc0d8a7e723f8f069698
chialam18 <phoebelam@u.nortwestern.edu>
2020-07-01 14:43:39 -0500

    method update

--- R/method.R
+++ R/method.R
@@ -24,19 +24,21 @@ method <- function (path) {
     gsub("/method extraction_| - method.csv", "", .) %>%
     gsub("/method extraction", "jp", .) -> dfnames
 
+  print(filenames)
+
   for (i in 1:length(filenames)) assign(dfnames[i], read.csv(filenames[i], na.strings=""))
 
   # if wide format
   #rename variable names
   jp %>%
-    rename_at(vars(!link.id), ~paste(., "_jp", sep="")) %>%
-    filter(is.na(link.id)==F) -> jp
+    dplyr::rename_at(dplyr::vars(!link.id), ~paste(., "_jp", sep="")) %>%
+    dplyr::filter(is.na(link.id)==F) -> jp
   kk %>%
-    rename_at(vars(!link.id), ~paste(., "_kk", sep="")) %>%
-    filter(is.na(link.id)==F)-> kk
+    dplyr::rename_at(dplyr::vars(!link.id), ~paste(., "_kk", sep="")) %>%
+    dplyr::filter(is.na(link.id)==F)-> kk
   ma %>%
-    rename_at(vars(!link.id), ~paste(., "_ma", sep="")) %>%
-    filter(is.na(link.id)==F)-> ma
+    dplyr::rename_at(dplyr::vars(!link.id), ~paste(., "_ma", sep="")) %>%
+    dplyr::filter(is.na(link.id)==F)-> ma
 
   #merge
   dat <- merge(merge(jp, kk, by = "link.id"), ma, by = "link.id")
@@ -54,130 +56,157 @@ method <- function (path) {
 
   #discrepancies
   dat %>%
-    mutate_at(vars(matches("assessment|timeframe|age|design|country|adiposity|female|white|black|latino|asian|other|psychiatric|physical|subgroup")),
+    dplyr::mutate_at(dplyr::vars(matches("sample.notes|assessment|timeframe|age|design|country|adiposity|female|white|black|latino|asian|other|psychiatric|physical|subgroup")),
               ~as.character(.)) %>%
-    mutate_at(vars(matches("assessment|timeframe|age|design|country|adiposity|female|white|black|latino|asian|other|psychiatric|physical|subgroup")),
-              ~case_when(is.na(.)==T~ "missing", TRUE~.))-> dat
+    dplyr::mutate_at(dplyr::vars(matches("assessment|timeframe|age|design|country|adiposity|female|white|black|latino|asian|other|psychiatric|physical|subgroup")),
+              ~dplyr::case_when(is.na(.)==T~ "missing", TRUE~.))-> dat
 
   dat %>%
-    filter(is.na(sample.notes_jp)==F & is.na(sample.notes_kk)==T |
+    dplyr::filter(is.na(sample.notes_jp)==F & is.na(sample.notes_kk)==T |
              is.na(sample.notes_jp)==F & is.na(sample.notes_ma)==T) %>%
-    select(link.id, sample.notes_jp, sample.notes_kk, sample.notes_ma) -> samplenotes
+    dplyr::select(link.id, sample.notes_jp, sample.notes_kk, sample.notes_ma) %>%
+    dplyr::mutate_at(dplyr::vars(sample.notes_jp:sample.notes_ma),
+                     ~dplyr::case_when(is.na(.)==T~"missing",
+                                       TRUE~.)) -> samplenotes
 
   dat %>%
-    filter(iv.assessment_jp != iv.assessment_kk |
+    dplyr::filter(iv.assessment_jp != iv.assessment_kk |
              iv.assessment_jp != iv.assessment_ma) %>%
-    select (link.id, iv.assessment_jp, iv.assessment_kk, iv.assessment_ma) -> iv.ass
+    dplyr::select (link.id, iv.assessment_jp, iv.assessment_kk, iv.assessment_ma) -> iv.ass
 
   dat %>%
-    filter(iv.timeframe_jp != iv.timeframe_kk |
+    dplyr::filter(iv.timeframe_jp != iv.timeframe_kk |
              iv.timeframe_jp != iv.timeframe_ma) %>%
-    select (link.id, iv.timeframe_jp, iv.timeframe_kk, iv.timeframe_ma) -> iv.time
+    dplyr::select (link.id, iv.timeframe_jp, iv.timeframe_kk, iv.timeframe_ma) -> iv.time
 
   dat %>%
-    filter(iv.mean.age_jp != iv.mean.age_kk |
+    dplyr::filter(iv.mean.age_jp != iv.mean.age_kk |
              iv.mean.age_jp != iv.mean.age_ma) %>%
-    select (link.id, iv.mean.age_jp, iv.mean.age_kk, iv.mean.age_ma) -> iv.agemean
+    dplyr::select (link.id, iv.mean.age_jp, iv.mean.age_kk, iv.mean.age_ma) -> iv.agemean
 
   dat %>%
-    filter(iv.age.sd_jp != iv.age.sd_kk |
+    dplyr::filter(iv.age.sd_jp != iv.age.sd_kk |
              iv.age.sd_jp != iv.age.sd_ma) %>%
-    select (link.id, iv.age.sd_jp, iv.age.sd_kk, iv.age.sd_ma) -> iv.agesd
+    dplyr::select (link.id, iv.age.sd_jp, iv.age.sd_kk, iv.age.sd_ma) -> iv.agesd
 
   dat %>%
-    filter(iv.age.range_jp != iv.age.range_kk |
+    dplyr::filter(iv.age.range_jp != iv.age.range_kk |
              iv.age.range_jp != iv.age.range_ma) %>%
-    select (link.id, iv.age.range_jp, iv.age.range_kk, iv.age.range_ma) -> iv.agerange
+    dplyr::select (link.id, iv.age.range_jp, iv.age.range_kk, iv.age.range_ma) -> iv.agerange
 
   dat %>%
-    filter(dv.assessment_jp != dv.assessment_kk |
+    dplyr::filter(dv.assessment_jp != dv.assessment_kk |
              dv.assessment_jp != dv.assessment_ma) %>%
-    select (link.id, dv.assessment_jp, dv.assessment_kk, dv.assessment_ma) -> dv.ass
+    dplyr::select (link.id, dv.assessment_jp, dv.assessment_kk, dv.assessment_ma) -> dv.ass
 
   dat %>%
-    filter(dv.timeframe_jp != dv.timeframe_kk |
+    dplyr::filter(dv.timeframe_jp != dv.timeframe_kk |
              dv.timeframe_jp != dv.timeframe_ma) %>%
-    select (link.id, dv.timeframe_jp, dv.timeframe_kk, dv.timeframe_ma) -> dv.time
+    dplyr::select (link.id, dv.timeframe_jp, dv.timeframe_kk, dv.timeframe_ma) -> dv.time
 
   dat %>%
-    filter(dv.mean.age_jp != dv.mean.age_kk |
+    dplyr::filter(dv.mean.age_jp != dv.mean.age_kk |
              dv.mean.age_jp != dv.mean.age_ma) %>%
-    select (link.id, dv.mean.age_jp, dv.mean.age_kk, dv.mean.age_ma) -> dv.agemean
+    dplyr::select (link.id, dv.mean.age_jp, dv.mean.age_kk, dv.mean.age_ma) -> dv.agemean
 
   dat %>%
-    filter(dv.age.sd_jp != dv.age.sd_kk |
+    dplyr::filter(dv.age.sd_jp != dv.age.sd_kk |
              dv.age.sd_jp != dv.age.sd_ma) %>%
-    select (link.id, dv.age.sd_jp, dv.age.sd_kk, dv.age.sd_ma) -> dv.agesd
+    dplyr::select (link.id, dv.age.sd_jp, dv.age.sd_kk, dv.age.sd_ma) -> dv.agesd
 
   dat %>%
-    filter(dv.age.range_jp != dv.age.range_kk |
+    dplyr::filter(dv.age.range_jp != dv.age.range_kk |
              dv.age.range_jp != dv.age.range_ma) %>%
-    select (link.id, dv.age.range_jp, dv.age.range_kk, dv.age.range_ma) -> dv.agerange
+    dplyr::select (link.id, dv.age.range_jp, dv.age.range_kk, dv.age.range_ma) -> dv.agerange
 
   dat %>%
-    filter(design_jp != design_kk |
+    dplyr::filter(design_jp != design_kk |
              design_jp != design_ma) %>%
-    select (link.id, design_jp, design_kk, design_ma) -> design
+    dplyr::select (link.id, design_jp, design_kk, design_ma) -> design
 
   dat %>%
-    filter(female.._jp != female.._kk |
+    dplyr::filter(female.._jp != female.._kk |
              female.._jp != female.._ma) %>%
-    select (link.id, female.._jp, female.._kk, female.._ma) -> female
+    dplyr::select (link.id, female.._jp, female.._kk, female.._ma) -> female
 
   dat %>%
-    filter(white.._jp != white.._kk |
+    dplyr::filter(white.._jp != white.._kk |
              white.._jp != white.._ma) %>%
-    select (link.id, white.._jp, white.._kk, white.._ma) -> white
+    dplyr::select (link.id, white.._jp, white.._kk, white.._ma) -> white
 
   dat %>%
-    filter(black.._jp != black.._kk |
+    dplyr::filter(black.._jp != black.._kk |
              black.._jp != black.._ma) %>%
-    select (link.id, black.._jp, black.._kk, black.._ma) -> black
+    dplyr::select (link.id, black.._jp, black.._kk, black.._ma) -> black
 
   dat %>%
-    filter(latino.._jp != latino.._kk |
+    dplyr::filter(latino.._jp != latino.._kk |
              latino.._jp != latino.._ma) %>%
-    select (link.id, latino.._jp, latino.._kk, latino.._ma) -> latino
+    dplyr::select (link.id, latino.._jp, latino.._kk, latino.._ma) -> latino
 
   dat %>%
-    filter(asian.._jp != asian.._kk |
+    dplyr::filter(asian.._jp != asian.._kk |
              asian.._jp != asian.._ma) %>%
-    select (link.id, asian.._jp, asian.._kk, asian.._ma) -> asian
+    dplyr::select (link.id, asian.._jp, asian.._kk, asian.._ma) -> asian
 
   dat %>%
-    filter(other.._jp != other.._kk |
+    dplyr::filter(other.._jp != other.._kk |
              other.._jp != other.._ma) %>%
-    select (link.id, other.._jp, other.._kk, other.._ma) -> other
+    dplyr::select (link.id, other.._jp, other.._kk, other.._ma) -> other
 
   dat %>%
-    filter(psychiatric.._jp != psychiatric.._kk |
+    dplyr::filter(psychiatric.._jp != psychiatric.._kk |
              psychiatric.._jp != psychiatric.._ma) %>%
-    select (link.id, psychiatric.._jp, psychiatric.._kk, psychiatric.._ma) -> psychiatric
+    dplyr::select (link.id, psychiatric.._jp, psychiatric.._kk, psychiatric.._ma) -> psychiatric
 
   dat %>%
-    filter(physical.._jp != physical.._kk |
+    dplyr::filter(physical.._jp != physical.._kk |
              physical.._jp != physical.._ma) %>%
-    select (link.id, physical.._jp, physical.._kk, physical.._ma) -> physical
+    dplyr::select (link.id, physical.._jp, physical.._kk, physical.._ma) -> physical
 
   dat %>%
-    filter(subgroup.n.a_jp != subgroup.n.a_kk |
+    dplyr::filter(subgroup.n.a_jp != subgroup.n.a_kk |
              subgroup.n.a_jp != subgroup.n.a_ma) %>%
-    select (link.id, subgroup.n.a_jp, subgroup.n.a_kk, subgroup.n.a_ma) -> subgroupna
+    dplyr::select (link.id, subgroup.n.a_jp, subgroup.n.a_kk, subgroup.n.a_ma) -> subgroupna
 
   dat %>%
-    filter(country_jp != country_kk |
+    dplyr::filter(country_jp != country_kk |
              country_jp != country_ma) %>%
-    select (link.id, country_jp, country_kk, country_ma) -> country
+    dplyr::select (link.id, country_jp, country_kk, country_ma) -> country
 
   dat %>%
-    filter(adiposity_jp != adiposity_kk |
+    dplyr::filter(adiposity_jp != adiposity_kk |
              adiposity_jp != adiposity_ma) %>%
-    select (link.id, adiposity_jp, adiposity_kk, adiposity_ma) -> adiposity
+    dplyr::select (link.id, adiposity_jp, adiposity_kk, adiposity_ma) -> adiposity
 
-  list (samplenotes, iv.ass, iv.time, iv.agemean, iv.agesd, iv.agerange, dv.ass, dv.time, dv.agemean, dv.agesd, dv.agerange,
-        design, female, white, black, latino, asian, other, psychiatric, physical, subgroupna, country, adiposity) -> summary
+  list ("sample notes" = samplenotes,
+        "iv.assessment"= iv.ass,
+        "iv.timeframe"= iv.time,
+        "iv.agemean" = iv.agemean,
+        "iv.agesd" = iv.agesd,
+        "iv.agerange" = iv.agerange,
+        "dv.assessment"= dv.ass,
+        "dv.timeframe"= dv.time,
+        "dv.agemean" = dv.agemean,
+        "dv.agesd" = dv.agesd,
+        "dv.agerange" = dv.agerange,
+        "design" = design,
+        "female" = female,
+        "white" = white,
+        "black" = black,
+        "latino"= latino,
+        "asian"= asian,
+        "other" = other,
+        "psychiatric" = psychiatric,
+        "physical"= physical,
+        "subgroupna" = subgroupna,
+        "country" = country,
+        "adiposity" = adiposity) -> summary
 
-  capture.output(summary, file = paste(path, "/method discrepancies_", format(Sys.Date(), format="%m.%d.%y"), ".txt", sep=""))
+  openxlsx::write.xlsx(summary, file = paste(path, "/method discrepancies_", format(Sys.Date(), format="%m.%d.%y"), ".xlsx", sep=""))
 
+  # capture.output(summary, file = paste(path, "/method discrepancies_", format(Sys.Date(), format="%m.%d.%y"), ".txt", sep=""))
+
+  print (sample(donzo, 1))
 }
 
